91991493898e1cf0996ec15668f592784a39ec0e
investordeedee <saejearsomsak@gmail.com>
2026-08-22 16:36:29 +0700

    ีupdate

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,14 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
-      lang="en"
+      lang="th"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
